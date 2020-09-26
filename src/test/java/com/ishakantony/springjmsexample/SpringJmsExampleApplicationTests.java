910ea3404bbcad1153e089127ad2af2cb37ad0ed
Ishak Antony Darmawan <ishak.antony@yahoo.com>
2020-09-26 20:54:17 +0800

    Setup project and dependencies

---
+++ src/test/java/com/ishakantony/springjmsexample/SpringJmsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishakantony.springjmsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJmsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
